c488ba4b7f9940b978eff51c1d0d36e22c239056
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-11-18 14:16:38 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.left = "180px"; // "180px"
+dodger.style.bottom = "0px"; // "0px"
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 400 - 180) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
